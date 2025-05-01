cbbffb4b991e399c4e7ee458e7bb595818e3d2a2
camilo guzman <jeysonkmguzman@gmail.com>
2025-05-01 15:29:59 -0500

    login

--- src/app/(auth)/lib/services/auth-services.ts
+++ src/app/(auth)/lib/services/auth-services.ts
@@ -38,6 +38,11 @@ export class AuthService {
         password: "",
         messageDB: error?.message ?? "",
       },
+      inputs:{
+        email:"",
+        password:"",
+        confirmPassword:""
+      }
     };
   }
 }
